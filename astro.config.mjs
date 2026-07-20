fc58f7ec3def49bfb02446acd5dede7d2e7b7506
Curro Diaz de Vivar <curro@mb360.es>
2026-07-20 11:50:34 +0200

    Build docs/ para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://clinicanuriaugarte.es',
+  outDir: 'docs',
+  site: 'https://currodiazdevivar.github.io/ClinicaNU/'
 });
