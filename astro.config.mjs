e378eeaa7b37992f317c713d2d570528b25e2faa
Curro Diaz de Vivar <curro@mb360.es>
2026-07-20 12:00:51 +0200

    Arreglar rutas CSS para GitHub Pages (base: /ClinicaNU)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   outDir: 'docs',
-  site: 'https://currodiazdevivar.github.io/ClinicaNU/'
+  site: 'https://currodiazdevivar.github.io',
+  base: '/ClinicaNU'
 });
